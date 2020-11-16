68b2f630c1adfa090854dac308b3eef8b477a297
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-16 04:05:24 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,10 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  
   puts phrase
+end
+
+def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts status
 end
 
